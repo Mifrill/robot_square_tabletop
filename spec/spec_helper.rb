214ea6b6eff80383406d11b8cbf2391b8dd434a2
mifrill <alexei.mifrill.strizhak@gmail.com>
2017-07-21 09:53:52 -0700

    add Simple cov setting for CI server

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,16 @@
 if ENV['ROBOT_ENV'] == 'test'
   require 'simplecov'
-  SimpleCov.start
-  puts 'required simplecov'
+  SimpleCov.minimum_coverage 100
+  SimpleCov.refuse_coverage_drop
+  puts 'CodeCoverage Enabled'
+  if ENV['CI']
+    require 'codeclimate-test-reporter'
+    require 'codecov'
+    SimpleCov.start
+    SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  else
+    SimpleCov.start
+  end
 end
 
 require './app/simulator'
