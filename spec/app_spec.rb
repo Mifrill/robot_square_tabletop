22cfeb16dd22218291a7fdb950054c9991e567d7
mifrill <alexei.mifrill.strizhak@gmail.com>
2017-07-22 23:26:21 -0700

    execute by hand STDIN

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -5,4 +5,7 @@ describe Toy do
   it 'Execute from file' do
     expect(Toy.execute_from_file('./example.json')).to eq(Toy.config['system_messages']['done'])
   end
+
+  # https://stackoverflow.com/questions/35737695/testing-ruby-with-rspec-for-file-name-and-stdin-with-while-gets
+  it 'Execute by hand'
 end
