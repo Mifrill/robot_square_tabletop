c6c2beb1fef0d2814a0c22d4bc4a9332b886b81d
Aleksey Strizhak <alexei.mifrill.strizhak@gmail.com>
2018-06-09 21:14:44 +0500

    refactoring: delegate by forwardable

--- app/simulator.rb
+++ app/simulator.rb
@@ -4,6 +4,13 @@ require_relative 'robot'
 
 module Toy
   class Simulator
+    require 'forwardable'
+
+    extend Forwardable
+
+    delegate %i(turn_left turn_right direction step) => :@robot
+    delegate %i(placed? position) => :@table
+
     attr_accessor :command
     PLACE_FAILED = SYSTEM_MESSAGES['place_failed']
 
@@ -38,28 +45,22 @@ module Toy
     end
 
     def move(_args = nil)
-      return PLACE_FAILED unless @table.placed?
-      position = @table.position
-      step     = @robot.step
-
+      return PLACE_FAILED unless placed?
       @table.place(position[:x] + step[:x], position[:y] + step[:y]) ? SYSTEM_MESSAGES['move_success'] : SYSTEM_MESSAGES['move_failed']
     end
 
     def left(_args = nil)
-      return PLACE_FAILED unless @table.placed?
-      @robot.turn_left
+      return PLACE_FAILED unless placed?
+      turn_left
     end
 
     def right(_args = nil)
-      return PLACE_FAILED unless @table.placed?
-      @robot.turn_right
+      return PLACE_FAILED unless placed?
+      turn_right
     end
 
     def report(_args = nil)
-      return PLACE_FAILED unless @table.placed?
-      position  = @table.position
-      direction = @robot.direction
-
+      return PLACE_FAILED unless placed?
       "#{position[:x]},#{position[:y]},#{direction.to_s.upcase}"
     end
   end
